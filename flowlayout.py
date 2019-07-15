ffd3b1774d93809aafd3ec8cc1a53500ff074070
resarenaldy <resarenaldy77@gmail.com>
2019-07-15 10:22:55 +0700

    Create flowlayout.py

---
+++ flowlayout.py
@@ -0,0 +1,28 @@
+while True :
+    width = int(input())
+    if width == 0 :
+        break
+    hl = True
+    hls1,hls2 = 0,0
+    panjang = 0
+    lebar = 0
+    while True :
+        p,l = map(int,input().split())
+        if p==-1 or l==-1 :
+            if hls1 < panjang:
+                hls1 = panjang
+            hls2 += lebar
+            break
+        if panjang + p <= width :
+            panjang += p
+            hl = True
+            if lebar < l :
+                lebar = l
+        else :
+            if hls1<panjang :
+                hls1 = panjang
+            hls2 += lebar
+            panjang = p
+            lebar = l
+    print(hls1,"x",hls2)
+
