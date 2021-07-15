e48af5204a206d07b84831bb7841ce326c73f985
Resa Renaldy <resarenaldy77@gmail.com>
2021-07-15 11:00:36 +0700

    Create thelastproblem.go

---
+++ thelastproblem.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	text := scanner.Text()
+	fmt.Printf("Thank you, %v, and farewell!", text)
+}
